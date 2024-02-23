f0910ab9f32a2b24431cdbfe64d7c87637a9ab13
sasheemi <s4sheemi@gmail.com>
2024-02-23 18:42:08 +0300

    Create insert_main_schema.sql

---
+++ База данных SQL/main schema/insert_main_schema.sql
@@ -0,0 +1,34 @@
+insert into main.countries(country)
+select distinct *
+	from (
+		select country
+		from sources.top100_kp
+		union 
+		select country
+		from sources.top500_kp
+		union
+		select country
+		from (select regexp_split_to_table(
+			  replace(replace(replace(country, '[', ''), ']', ''), '''', ''),
+			  ', ') 
+			  as country
+			  from sources.top250)
+		);
+
+insert into main.genres(genre)
+select distinct *
+	from (
+		select genre
+		from sources.top100_kp
+		union 
+		select genre
+		from sources.top500_kp
+		union
+		select genre
+		from (select regexp_split_to_table(
+			  replace(replace(replace(genre, '[', ''), ']', ''), '''', ''),
+			  ', ') 
+			  as genre
+			  from sources.top250)
+		);
+
